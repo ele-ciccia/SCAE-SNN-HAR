f34fe7eaf3722670452f6efb66670526e2c95b76
Eleonora Cicciarella <eleonora.cicciarella@phd.unipd.it>
2024-06-10 23:28:42 +0200

    Finished train

--- LateX_plot_params.js
+++ LateX_plot_params.js
@@ -1,11 +1,11 @@
 plt.rcParams.update({
-    'axes.labelsize': 28.0,
+    'axes.labelsize': 30.0,
     'grid.alpha': 0.6,
     'legend.framealpha': 0.6,
     "text.usetex": True,
     "font.family": "serif",
     'figure.figsize': [15,6],
-    "font.size": 28,
+    "font.size": 30,
     "hatch.linewidth": 0.0,
     "hatch.color": (0,0,0,0.0),
     "axes.prop_cycle": cycler(color=sns.color_palette("tab10"))
